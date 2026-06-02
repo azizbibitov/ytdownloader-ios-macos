5d2383c1b9db13934850fd1db828fb576e7703ff
Aziz Bibitov <azizbibitov11@gmail.com>
2026-06-02 15:20:41 +0500

    Initial Commit

---
+++ youtube-downloader/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  youtube-downloader
+//
+//  Created by Aziz Bibitov on 02.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ youtube-downloaderTests/youtube_downloaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  youtube_downloaderTests.swift
+//  youtube-downloaderTests
+//
+//  Created by Aziz Bibitov on 02.06.2026.
+//
+
+import Testing
+
+struct youtube_downloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
